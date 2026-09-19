87b303194770cd35cac6a56071fb8f7bc4b84672
Deanne Smeaton <deanne.smeaton@gmail.com>
2026-09-19 23:28:11 +1200

    Overall make over of the site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Deanne Smeaton';
+export const SITE_DESCRIPTION = 'Building software, solving problems, learning along the way.';
+export const SITE_EMAIL = "hello@deannesmeaton.co.nz"
+export const SITE_LINKEDIN = "https://www.linkedin.com/in/smeatond"
+export const SITE_GITHUB = "https://github.com/smeatond"
+export const SITE_X = "https://x.com/deanne_smeaton"
